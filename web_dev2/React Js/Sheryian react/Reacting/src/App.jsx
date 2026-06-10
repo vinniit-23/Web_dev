705fa312028a2b160e5f513e5d8f163b4c67012e
vinit <vinitpandey2306@gmail.com>
2026-06-10 21:19:14 +0530

    learning props with useState

--- web_dev2/React Js/Sheryian react/Reacting/src/App.jsx
+++ web_dev2/React Js/Sheryian react/Reacting/src/App.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 // import Carousel from "./Component/Carousel";
 import Card3 from "./Component/Card3";
 
 function App() {
+  const rawData = [
+  
+    {name:"John",profession:"Artist",url:"",friend:false},
+    {name:"Jacob",profession:"Lawyer",url:"",friend:false},
+    {name:"Sam",profession:"Researcher",url:"",friend:false},
+    {name:"Mark",profession:"Coder",url:"",friend:false},
+    { name: "Peter", profession: "Investor", url: "", friend: false },
+    
+  ]
+
+  const [data, setData] = useState(rawData);
+  const handleEventButton = (Elemindex) => {
+    return setData((prev) => {
+      prev.map((item, index) => {
+        if (index===Elemindex) {
+          return {...item,friend:!item.friend}
+        }
+        return item;
+     })
+   })
+
+  }
+
   return (
     // <Carousel/>
-    <div className="m-5">
-      <Card3 name={'Know More'} color={"bg-blue-500"} />
-      <Card3 name={"Download"} color={"bg-red-500"} />
-    </div>
+    <div className=" bg-zinc-400">
+      {data.map((item, index) => {
+        <Card3  index={index} value={item}  key={index} handleEventButton={handleEventButton} />
+      })}
+      </div>
   );
 }
 
--- web_dev2/React Js/Sheryian react/Reacting/src/Component/Card3.jsx
+++ web_dev2/React Js/Sheryian react/Reacting/src/Component/Card3.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-export default function Card3({name,color}) {
-    return <>
-        <button className={`${color} px-3 py-2 rounded text-sm`}>{name}</button>
-    </>  
+export default function Card3({ value, index, handleEventButton }) {
+  const { name, profession, url, friend } = value;
+
+  return (
+    <div className="w-44 h-54 rounded overflow-hidden m-4">
+      <div className="w-full h-32 bg-sky-500 object-cover">
+              <img
+                  className=""
+                  src={url} alt="" />
+      </div>
+      <div>
+        <h2 className="text-2xl ">{name}</h2>
+        <h3 className="text-xl ">{profession}</h3>
+        <button
+          onClick={() => {
+            handleEventButton(index);
+          }}
+          className={`rounded px-3 py-2 text-xs ${friend?"bg-red-600":"bg-blue-600"}`}
+        >
+          {friend ? "Remove Friend" : "Add Friend"}
+        </button>
+      </div>
+    </div>
+  );
 }
